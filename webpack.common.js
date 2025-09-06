d3d5e21a238d5fd0dddcfec2548f5723dbd6e763
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2025-09-06 12:09:37 +0000

    feat: add a major revamp

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './assets/js/main.js',
+    app: './assets/js/main-scripts.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './assets/js/main.js',
+    filename: './assets/js/main-scripts.js',
   },
 };
